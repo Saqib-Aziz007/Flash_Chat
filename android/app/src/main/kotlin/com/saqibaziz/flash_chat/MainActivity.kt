2d3e8c0c9eb03769e155d9fabbfee2531adeba17
Muhammad Saqib Aziz <saqib.aziz1000@gmail.com>
2021-08-29 11:48:04 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/saqibaziz/flash_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.saqibaziz.flash_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat/components/reuseable_button.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const id = 'Welcome_screen';
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller =
+        AnimationController(duration: Duration(seconds: 2), vsync: this);
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60.0,
+                  ),
+                ),
+                // ignore: deprecated_member_use
+                TypewriterAnimatedTextKit(
+                  text: ['Flash Chat'],
+                  textStyle: TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            reuseable_button(Colors.lightBlueAccent, 'Login', () {
+              Navigator.pushNamed(context, LoginScreen.id);
+            }),
+            reuseable_button(Colors.blueAccent, 'Register', () {
+              Navigator.pushNamed(context, RegistrationScreen.id);
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+}
